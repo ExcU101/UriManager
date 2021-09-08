8a53bd8676e11b3f875dc2cfb576a6616de39c7b
MAGIC CONTROL <magic25control@gmail.com>
2021-09-08 19:57:22 +0300

    Upload

--- core/src/main/java/com/excu_fcd/core/data/Item.kt
+++ core/src/main/java/com/excu_fcd/core/data/Item.kt
@@ -1,10 +1,11 @@
 package com.excu_fcd.core.data
 
 import android.os.Parcelable
+import kotlin.random.Random
 
 interface Item : Parcelable, Comparable<Item> {
 
-    fun getId(): Int
+    fun getId(): Int = Random.nextInt()
 
     fun getName(): String
 
--- core/src/main/java/com/excu_fcd/core/provider/LocalProvider.kt
+++ core/src/main/java/com/excu_fcd/core/provider/LocalProvider.kt
@@ -1,9 +1,20 @@
 package com.excu_fcd.core.provider
 
-class LocalProvider : Provider, Subscriber {
+import android.content.Context
+import android.os.Parcelable
+import androidx.core.app.NotificationManagerCompat
+import com.excu_fcd.core.utils.getNotificationManager
+import kotlinx.parcelize.Parceler
+import kotlinx.parcelize.Parcelize
+import kotlinx.parcelize.WriteWith
+
+class LocalProvider(private val context: Context) : Provider,
+    Subscriber<Any> {
+
+    private val manager: NotificationManagerCompat = context.getNotificationManager()
 
     override fun subscribe() {
-        TODO("Not yet implemented")
+
     }
 
     override fun unsubscribe() {
